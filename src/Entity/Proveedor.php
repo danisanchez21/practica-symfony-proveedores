52e252aeaeb53d273501d2de4737c4b76d7eaafe
Daniel Sanchez Aránega <sanchez126e@gmail.com>
2025-06-16 18:03:19 +0200

    feat: creada la entidad Proveedor

---
+++ src/Entity/Proveedor.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProveedorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProveedorRepository::class)
+ */
+class Proveedor
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $telefono;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $tipo;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $activo;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creadoEn;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $actualizadoEn;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?string
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(string $telefono): self
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function getTipo(): ?string
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo(string $tipo): self
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    public function getActivo(): ?bool
+    {
+        return $this->activo;
+    }
+
+    public function setActivo(bool $activo): self
+    {
+        $this->activo = $activo;
+
+        return $this;
+    }
+
+    public function getCreadoEn(): ?\DateTimeInterface
+    {
+        return $this->creadoEn;
+    }
+
+    public function setCreadoEn(\DateTimeInterface $creadoEn): self
+    {
+        $this->creadoEn = $creadoEn;
+
+        return $this;
+    }
+
+    public function getActualizadoEn(): ?\DateTimeInterface
+    {
+        return $this->actualizadoEn;
+    }
+
+    public function setActualizadoEn(\DateTimeInterface $actualizadoEn): self
+    {
+        $this->actualizadoEn = $actualizadoEn;
+
+        return $this;
+    }
+}
