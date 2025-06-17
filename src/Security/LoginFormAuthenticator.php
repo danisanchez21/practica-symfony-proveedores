2b954c99cee5a53bf3f195efb66d19ad8dfb0504
Daniel Sanchez Aránega <sanchez126e@gmail.com>
2025-06-17 18:35:24 +0200

    feat: creado sistema de autenticación de Usuario y LoginFormAuthenticator

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class LoginFormAuthenticator extends AbstractGuardAuthenticator
+{
+    private $entityManager;
+    private $passwordEncoder;
+    private $urlGenerator;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder, UrlGeneratorInterface $urlGenerator)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordEncoder = $passwordEncoder;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function supports(Request $request)
+    {
+        return $request->attributes->get('_route') === 'login' && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $email = $request->request->get('_email');
+        $password = $request->request->get('_password');
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return [
+            'email' => $email,
+            'password' => $password,
+        ];
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        return $this->entityManager->getRepository(Usuario::class)
+            ->findOneBy(['email' => $credentials['email']]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return new RedirectResponse($this->urlGenerator->generate('proveedor_listar'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        return new RedirectResponse($this->urlGenerator->generate('login'));
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new RedirectResponse($this->urlGenerator->generate('login'));
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
