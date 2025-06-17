33cddeea559e0388d613a76645eeb4681a98198e
Daniel Sanchez Aránega <sanchez126e@gmail.com>
2025-06-17 19:07:01 +0200

    feat: creada la vista Twig y el controlador de seguridad para la autenticación

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // obtiene el posible error de login si existe
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // último email introducido por el usuario
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(): void
+    {
+        // Symfony intercepta esta ruta automáticamente
+        throw new \Exception('No olvides activar el logout en security.yaml');
+    }
+}
